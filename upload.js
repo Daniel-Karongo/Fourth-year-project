931e1dfdef97480ce6235076d1872b3337462312
Daniel-Karongo <danieltorrent001@gmail.com>
2023-05-27 23:42:18 +0300

    fifth commit

--- upload.js
+++ upload.js
@@ -203,12 +203,14 @@ function validateForm(event) {
     const apartmentBedrooms = document.getElementById('number-of-apartment-bedrooms');
     const moreApartmentBedrooms = document.getElementById('more-apartment-bedrooms');
 
-    const moreHouseBedrooms = document.getElementById('more-house-bedrooms');
     const houseBedrooms = document.getElementById('number-of-house-bedrooms');
-
+    const moreHouseBedrooms = document.getElementById('more-house-bedrooms');
+    
     const businessPremise = document.getElementById('type-of-premise');
 
     const numberOfRentals = document.getElementById('number-of-available-rentals');
+    const maximumNumberOfOccupants = document.getElementById('maximum-occupants');
+
     const location = document.getElementById('location');
     const rentalTerm = document.getElementById('rental-term');
     const amountOfRent = document.getElementById('rent');
@@ -219,10 +221,10 @@ function validateForm(event) {
 
     function displayError(element, errorMessage) {
         let parentDiv = element.parentElement;
-        let error = parentDiv.querySelector('.error');
-        error.innerHTML = errorMessage;        
+        let error = parentDiv.querySelector('.error');                
         parentDiv.classList.add('error');
-        parentDiv.classList.remove('success');             
+        parentDiv.classList.remove('success');
+        error.innerHTML = errorMessage;             
     }
 
     function successfulEntry(element) {
@@ -235,6 +237,7 @@ function validateForm(event) {
 
     function formValidator() {
         const rentalTypeValue = rentalType.value;
+        const maximumNumberOfOccupantsValue = maximumNumberOfOccupants.value;
         const numberOfRentalsValue = numberOfRentals.value;
         const locationValue = location.value;
         const rentalTermValue = rentalTerm.value;
@@ -250,50 +253,115 @@ function validateForm(event) {
         const businessPremiseValue = businessPremise.value;
         
               
-        
-
         if(rentalTypeValue === 'no-value') {
             displayError(rentalType, "Please Choose A Type of Rental");
-        } else if ((rentalTypeValue === 'Hostel') || (rentalTypeValue === 'Bedsitter') || 
-        (rentalTypeValue === 'Single Room') || 
-        (rentalTypeValue === 'Suite')) {
+        } 
+        
+        
+        else if ((rentalTypeValue === 'Hostel') || (rentalTypeValue === 'Bedsitter') || (rentalTypeValue === 'Single Room') || (rentalTypeValue === 'Suite')) {
             if (numberOfRentalsValue === '') {
                 displayError(numberOfRentals, "Please Specify the Number of " + rentalTypeValue + "s"); 
                 successfulEntry(rentalType);
+                if (maximumNumberOfOccupantsValue === '') {
+                    displayError(maximumNumberOfOccupants, "Please Specify the Maximum Number of Occupants For Each " + rentalTypeValue);                  
+                } else {                    
+                    successfulEntry(maximumNumberOfOccupants);
+                }
+
             } else {
+                successfulEntry(rentalType);
                 successfulEntry(numberOfRentals);
+                if (maximumNumberOfOccupantsValue === '') {
+                    displayError(maximumNumberOfOccupants, "Please Specify the Maximum Number of Occupants For Each " + rentalTypeValue);                  
+                } else {                    
+                    successfulEntry(maximumNumberOfOccupants);
+                }
             }
-        } else if (rentalTypeValue === 'Apartment') {
-            if (apartmentBedroomsValue === 'no-value') {
-                displayError(apartmentBedrooms, "Please Specify the Number of Bedrooms in Each " + rentalTypeValue);                
-            } else if (apartmentBedroomsValue === 'more') {
-                if (moreApartmentBedroomsValue === '') {
-                    displayError(moreApartmentBedrooms, "Please Specify the Number of Bedrooms in Each " + rentalTypeValue);
+        } 
+        
+        
+        else if (rentalTypeValue === 'Apartment') {
+            if (apartmentBedroomsValue === 'no-value') {    // No "Number of Bedrooms" Option Chosen
+                displayError(apartmentBedrooms, "Please Specify the Number of Bedrooms in Each " + rentalTypeValue);
+                successfulEntry(rentalType);                
+            } 
+            
+            else if (apartmentBedroomsValue === 'more') {   // "More" option is chosen
+                if (moreApartmentBedroomsValue == '') {
+                    displayError(moreApartmentBedrooms, "Please Specify the Number of Bedrooms in Each " + rentalTypeValue); successfulEntry(rentalType);
+                    successfulEntry(apartmentBedrooms);                    
                 } else {
+                    successfulEntry(rentalType);
+                    successfulEntry(apartmentBedrooms);
                     successfulEntry(moreApartmentBedrooms);
+                    console.log(numberOfRentalsValue);
+                    if (numberOfRentalsValue === '') {
+                        displayError(numberOfRentals, "Please Specify the Number of " + moreApartmentBedroomsValue + "-Bedroom Apartments");                   
+                    } else {                    
+                        successfulEntry(numberOfRentals);
+                    }
                 }
-            } else {
+            } 
+            
+            else {      // For "1/2/3 - Bedroom" Apartments
+                successfulEntry(rentalType);
                 successfulEntry(apartmentBedrooms);
-            }
-        } else if (rentalTypeValue === 'Business Premise') {
-            if (businessPremiseValue === 'no-value') {
-                displayError(businessPremise, "Please Specify the Type Of Business Premise");                
-            } else {
-                successfulEntry(businessPremise);
-            }
-        } else {
-            // const houseBedroomsValue = houseBedrooms.value;
-            // const moreHouseBedroomsValue = moreHouseBedrooms.value;
-            if (houseBedroomsValue === 'no-value') {
-                displayError(houseBedrooms, "Please Specify the Number of Bedrooms in Each " + rentalTypeValue);                
-            } else if (houseBedroomsValue === 'more') {
-                if (moreHouseBedroomsValue === '') {
-                    displayError(moreHouseBedrooms, "Please Specify the Number of Bedrooms in Each " + rentalTypeValue);
-                } else {
-                    successfulEntry(moreHouseBedrooms);
+                if (numberOfRentalsValue === '') {
+                    displayError(numberOfRentals, "Please Specify the Number of " + apartmentBedroomsValue + " Apartments");                   
+                } else {                    
+                    successfulEntry(numberOfRentals);
                 }
+            }
+        } 
+        
+        
+        else if (rentalTypeValue === 'Business Premise') {
+            if (businessPremiseValue === 'no-value') {
+                displayError(businessPremise, "Please Specify the Type Of Business Premise");
+                successfulEntry(rentalType);                
             } else {
+                successfulEntry(rentalType);
+                successfulEntry(businessPremise);
+                if (numberOfRentalsValue === '') {
+                    displayError(numberOfRentals, "Please Specify the Number of " + businessPremiseValue + "s");                   
+                } else {                    
+                    successfulEntry(numberOfRentals);
+                }
+            }
+        } 
+        
+
+        else {      // For "House"
+            if (houseBedroomsValue === 'no-value') {       // For "No-value" Option
+                displayError(houseBedrooms, "Please Specify the Number of Bedrooms in Each " + rentalTypeValue);
+                successfulEntry(rentalType);
+
+
+            } else if (houseBedroomsValue === 'more') {     // For "More"
+                if (moreHouseBedroomsValue == '') {
+                    displayError(moreHouseBedrooms, "Please Specify the Number of Bedrooms In Each " + rentalTypeValue);
+                    successfulEntry(rentalType);
+                    successfulEntry(houseBedrooms);                    
+                } else {
+                    successfulEntry(rentalType);
+                    successfulEntry(houseBedrooms);
+                    successfulEntry(moreHouseBedrooms);
+                    if (numberOfRentalsValue === '') {
+                        displayError(numberOfRentals, "Please Specify the Number of " + moreHouseBedroomsValue + "-Bedroom Houses");                   
+                    } else {                    
+                        successfulEntry(numberOfRentals);
+                    }                    
+                }
+
+
+            } else {        // For "1/2/3"-Bedroom Houses
+                successfulEntry(rentalType);
                 successfulEntry(houseBedrooms);
+                if (numberOfRentalsValue === '') {
+                    displayError(numberOfRentals, "Please Specify the Number of " + houseBedroomsValue + " Houses");                   
+                } else {                    
+                    successfulEntry(numberOfRentals);
+                }
             }
         }
 
@@ -305,17 +373,20 @@ function validateForm(event) {
             successfulEntry(location);
         }
 
+
         if(rentalTermValue === 'no-value') {
             displayError(rentalTerm, "Please Specify How Often The Rent Should Be Paid");
         } else {
             successfulEntry(rentalTerm);
         }
 
+
         if(amountOfRentValue === '') {
             displayError(amountOfRent, "Please Specify How Much Rent Should Be Paid For Each Term");
         } else {
             successfulEntry(amountOfRent);
         }
+
 
         if(imagesValue.length === 0) {
             displayError(imagesLabel, "Please Upload Photographs of the " + rentalTermValue + "s");
