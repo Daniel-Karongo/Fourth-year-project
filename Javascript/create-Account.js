2983293cdd2eee78a54a70425e00c907fa57c5c3
Daniel-Karongo <danieltorrent001@gmail.com>
2023-06-07 09:40:57 +0300

    seventh-commit

--- Javascript/create-Account.js
+++ Javascript/create-Account.js
@@ -41,3 +41,30 @@ function validateEmail() {
         displayError(email, "Please Enter a Valid Email");
     }
 }
+
+function validateField(id, error) {
+    const field = document.getElementById(id);
+    const fieldValue = field.value;
+
+    if(fieldValue === '') {
+        displayError(field, error);
+    } else {
+        nullifySuccessOrFailure(field);
+    }
+}
+
+function validatePhoneNumber() {
+    const phoneNumber = document.getElementById('phone-number');
+    const phoneNumberValue = phoneNumber.value;
+
+    if(phoneNumberValue === '') {
+        displayError(phoneNumber, "Please Specify A Phone Number that will be Associated With Your Rentals");
+    } else {
+        console.log(typeof phoneNumberValue.length);
+        if(phoneNumberValue.length !== 10) {
+            displayError(phoneNumber, "Please Enter A Valid Phone Number");
+        } else {
+            nullifySuccessOrFailure(phoneNumber);
+        }        
+    }
+}
